66154b34102487ec9bd1babf951e051f3ad11ed6
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2020-09-28 12:09:47 +0200

    comma dangle and jsx wrap added

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ module.exports = {
     }],
     "quotes": [2, "single"],
     "jsx-quotes": ["error", "prefer-double"],
-    "react/jsx-curly-spacing": [2, { "when": "never", "allowMultiline": false }]
+    "react/jsx-curly-spacing": [2, { "when": "never", "allowMultiline": false }],
+    "comma-dangle": ["error", "never"],
+    "react/jsx-wrap-multilines": ["error", {
+      "declaration": "parens-new-line",
+      "assignment": "parens-new-line",
+      "return": "parens-new-line",
+      "arrow": "parens-new-line",
+      "condition": "parens-new-line",
+      "logical": "ignore",
+      "prop": "ignore"
+    }]
   }
 }
