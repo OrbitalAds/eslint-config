722df2274daad69386e1996493e4168a07f3c5d6
Marc Fuentes Montenegro <marcf.montenegro@gmail.com>
2021-04-12 12:51:10 +0200

    get rid of typescript plugin

--- index.js
+++ index.js
@@ -3,35 +3,28 @@ module.exports = {
     MyGlobal: true
   },
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended"
-  ],
+  extends: [ "plugin:react/recommended" ],
   settings: {
     react: {
       version: "detect"
     }
   },
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "modules": true
+    }
+  },
   rules: {
     "eol-last": ["error", "always"],
-    semi: ["error", "never"],
+    "semi": ["error", "never"],
     "object-curly-spacing": ["error", "always"],
     "react/prop-types": 0,
     "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 0 }],
-    "@typescript-eslint/no-explicit-any": "off",
     "react/no-unescaped-entities": "off",
     "keyword-spacing": ["error", { "before": true }],
     "template-curly-spacing": ["error", "never"],
-    "@typescript-eslint/member-delimiter-style": ["error", {
-      multiline: {
-        delimiter: "none",
-        requireLast: false
-      },
-      singleline: {
-        delimiter: "comma",
-        requireLast: false
-      }
-    }],
     "quotes": [2, "single"],
     "jsx-quotes": ["error", "prefer-double"],
     "react/jsx-curly-spacing": [2, { "when": "never", "allowMultiline": false }],
